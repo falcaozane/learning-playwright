683a72c75966b1cfaf0db6336083196b05d8b54f
falcaozane <falcaozane@gmail.com>
2026-03-30 14:58:22 +0530

    section 6 :2 videos done

--- tests/client.spec.js
+++ tests/client.spec.js
@@ -5,16 +5,31 @@ const { test, expect } = require('@playwright/test');
  
 test('@Web Client App login', async ({ page }) => {
    //js file- Login js, DashboardPage
-   const email = "anshika@gmail.com";
-   const productName = 'zara coat 3';
+   const email = "zanefalcao21@gmail.com";
+   const productName = 'ZARA COAT 3';
    const products = page.locator(".card-body");
    await page.goto("https://rahulshettyacademy.com/client");
    await page.locator("#userEmail").fill(email);
-   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("#userPassword").fill("Zane210803#");
    await page.locator("[value='Login']").click();
    await page.waitForLoadState('networkidle');
    await page.locator(".card-body b").first().waitFor();
    const titles = await page.locator(".card-body b").allTextContents();
    console.log(titles);
- 
+   const productsCount = await products.count();
+
+   for (let i = 0; i < productsCount; ++i) {
+      if (await products.nth(i).locator("b").textContent() === productName) {
+         // add to cart
+         await products.nth(i).locator("text= Add To Cart").click();
+         break;
+      }
+   }
+
+   await page.locator("[routerlink*='cart']").click();
+   await page.locator("div li").first().waitFor();
+   const bool = await page.locator(`h3:has-text("${productName}")`).isVisible();
+   expect(bool).toBeTruthy();
+
+   await page.pause();
 })
