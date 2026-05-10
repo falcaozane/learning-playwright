a66292e6d4d579c2ec8d3ca71dc9e7a8fc27dfd6
falcaozane <falcaozane@gmail.com>
2026-05-10 12:46:34 +0530

    starting with web api testing

---
+++ tests/WebAPIPart1.spec.js
@@ -0,0 +1,57 @@
+const { test, expect , request} = require('@playwright/test');
+
+const loginPayload = {userEmail : "anshika@gmail.com", userPassword : "Iamking@000"};
+
+test.beforeAll( async()=>{
+
+    const apiContext = await request.newContext();
+
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",{
+        data : loginPayload
+    })
+
+    expect(loginResponse.ok()).toBeTruthy();
+    const loginResponseJson = await loginResponse.json();
+    console.log(loginResponseJson.token);
+    console.log(loginResponseJson.userId);
+    console.log(loginResponseJson.message);
+
+});
+
+test.beforeEach(()=>{
+
+})
+ 
+ 
+ 
+ 
+test('@Webst Client App login', async ({ page }) => {
+   //js file- Login js, DashboardPage
+   const email = "anshika@gmail.com";
+   const productName = 'ZARA COAT 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.getByPlaceholder("email@example.com").fill(email);
+   await page.getByPlaceholder("enter your passsword").fill("Iamking@000");
+   await page.getByRole('button',{name:"Login"}).click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   
+   await page.locator(".card-body").filter({hasText:"ZARA COAT 3"})
+   .getByRole("button",{name:"Add to Cart"}).click();
+ 
+   await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+ 
+   //await page.pause();
+   await page.locator("div li").first().waitFor();
+   await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+ 
+   await page.getByRole("button",{name :"Checkout"}).click();
+ 
+   await page.getByPlaceholder("Select Country").pressSequentially("ind");
+ 
+   await page.getByRole("button",{name :"India"}).nth(1).click();
+   await page.getByText("PLACE ORDER").click();
+ 
+   await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+})
